1317b448e0fe803ff4ea4ccd1d8a1b3e7b5dbb3e
Vladimir Ilimurzin <ilimurzin@ya.ru>
2025-04-14 18:15:26 +0300

    Шаблоны для встройки в карточку

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -8,6 +8,7 @@ use Bitrix24\SDK\Application\Requests\Placement\PlacementRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AppController extends AbstractController
@@ -20,6 +21,20 @@ final class AppController extends AbstractController
     public function handle(Request $request): Response
     {
         $placementRequest = new PlacementRequest($request);
+
+        if ($placementRequest->getCode() === 'USERFIELD_TYPE') {
+            $mode = $placementRequest->getPlacementOptions()['MODE'] ?? '';
+
+            if ($mode === 'view') {
+                return $this->render('uf/view.html.twig');
+            }
+
+            if ($mode === 'edit') {
+                return $this->render('uf/edit.html.twig');
+            }
+
+            throw new BadRequestHttpException("Unsupported MODE `{$mode}`");
+        }
 
         return $this->render('index.html.twig', [
             'code' => $placementRequest->getCode(),
