8dacc6664cf4f8bb49ec9130d015bfceaeb46d25
Vladimir Ilimurzin <ilimurzin@ya.ru>
2025-07-09 09:34:02 +0300

    Больше данных

--- src/BanksConverter.php
+++ src/BanksConverter.php
@@ -23,6 +23,54 @@ final readonly class BanksConverter
                 'name' => (string) $participantInfo['NameP'],
             ];
 
+            if (isset($participantInfo['EnglName'])) {
+                $bank['nameInEnglish'] = (string) $participantInfo['EnglName'];
+            }
+
+            if (isset($participantInfo['RegN'])) {
+                $bank['registryNumber'] = (string) $participantInfo['RegN'];
+            }
+
+            $postalIndex = isset($participantInfo['Ind']) ? ((string) $participantInfo['Ind']) : null;
+            $localityType = isset($participantInfo['Tnp']) ? ((string) $participantInfo['Tnp']) : null;
+            $localityName = isset($participantInfo['Nnp']) ? ((string) $participantInfo['Nnp']) : null;
+            $address = isset($participantInfo['Adr']) ? ((string) $participantInfo['Adr']) : null;
+
+            $addressCombined = implode(
+                ', ',
+                array_filter([
+                    $postalIndex,
+                    implode(
+                        ' ',
+                        array_filter([
+                            $localityType,
+                            $localityName,
+                        ]),
+                    ),
+                    $address,
+                ]),
+            );
+
+            if ($postalIndex) {
+                $bank['postalIndex'] = $postalIndex;
+            }
+
+            if ($localityType) {
+                $bank['localityType'] = $localityType;
+            }
+
+            if ($localityName) {
+                $bank['localityName'] = $localityName;
+            }
+
+            if ($address) {
+                $bank['address'] = $address;
+            }
+
+            if ($addressCombined) {
+                $bank['addressCombined'] = $addressCombined;
+            }
+
             $banks[] = $bank;
         }
 
